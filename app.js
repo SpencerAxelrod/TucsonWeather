e09dac2f44798e8e344ad85b94a2ec2fe74ad0d7
Spencer Axelrod <spencer.axelrod@gmail.com>
2020-06-24 22:50:33 -0700

    Source is prepared to start CSS art

--- app.js
+++ app.js
@@ -5,46 +5,50 @@ window.addEventListener('load', ()=> {
     /* Hide key from public source in a .gitignore'd config.js file.  */
     let key = config.OPENWEATHER_KEY;
 
-    let temperatureDescritption = document.querySelector('.temperature-description');
+    let temperatureDegreeDiv = document.querySelector('.temperature-degree_div');
     let temperatureDegreeValue = document.querySelector('.temperature-degree_value');
+    let temperatureDegreeUnit = document.querySelector('.temperature-degree_unit');
+    let temperatureDescritption = document.querySelector('.temperature-description');
     let locationTimezone = document.querySelector('.location-timezone');
     let weatherIcon = document.querySelector('.weather-icon');
 
-    if(navigator.geolocation){
+    function getTucsonWeatherData( ) {
+        lat = 32.2226;
+        lon = -110.9747;
 
-        function success(pos) {
-            lon = pos.coords.longitude;
-            lat = pos.coords.latitude;
+        const openweather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`
+        fetch(openweather)
+            .then(response =>{
+                console.log(response);
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
 
-            const openweather = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`
-            fetch(openweather)
-                .then(response =>{
-                    console.log(response);
-                    return response.json();
+
+                const { temp, weather } = data.current;
+
+                let temp_K = temp;
+                let temp_C = Math.round(temp_K - 273.15);
+                let temp_F = Math.round( (temp_K - 273.15) * (9 / 5) + 32);
+
+                temperatureDegreeValue.textContent = temp_F;
+                temperatureDescritption.textContent = weather[0].main;
+                weatherIcon.src = "https://openweathermap.org/img/wn/" + weather[0].icon + "@2x.png";
+
+                temperatureDegreeDiv.addEventListener('click', () => {
+                    if (temperatureDegreeUnit.textContent === "F") {
+                        temperatureDegreeUnit.textContent = "C"
+                        temperatureDegreeValue.textContent = temp_C;
+                    } else {
+                        temperatureDegreeUnit.textContent = "F"
+                        temperatureDegreeValue.textContent = temp_F;
+                    }
                 })
-                .then(data => {
-                    console.log(data);
-                    const { temp, weather } = data.current;
 
-                    temperatureDegreeValue.textContent = Math.round( (temp - 273.15) * (9 / 5) + 32);
-                    temperatureDescritption.textContent = weather[0].main;
-                    locationTimezone.textContent = data.timezone;
-                    weatherIcon.src = "https://openweathermap.org/img/wn/" + weather[0].icon + "@2x.png";
+            });
+    };
 
-                });
-        }
-
-        function error(err) {
-            console.warn(`ERROR(${err.code}): ${err.message}`);
-        }
-
-        var options = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        };
-
-        navigator.geolocation.getCurrentPosition(success, error, options);
+    getTucsonWeatherData();
         
-    }
 });
